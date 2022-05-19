2dd7929cc2d497512c3d0f4de8b5e7363c7e71d8
ishanshastri <94653377+ishanshastri@users.noreply.github.com>
2022-05-19 00:46:13 -0400

    Add files via upload

---
+++ ExpertSystem/Interfaces/IFact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpertSystem_2
+    {
+    interface IFact
+        {
+        bool GetBoolValue();
+        int GetIntValue();
+        double GetDoubleValue();
+        string GetStringValue();
+
+        void SetValue(bool value);
+        void SetValue(int value);
+        void SetValue(double value);
+        void SetValue(string value);
+        }
+    }
